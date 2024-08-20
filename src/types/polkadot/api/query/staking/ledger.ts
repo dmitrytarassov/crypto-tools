93cb6d542213d041b4519eba066bde4d877d4d5e
Dmitriy Tarasov <dm.tarassov@yandex.ru>
2024-08-20 17:38:34 +0300

    update api.query.staking.ledger return type

--- src/types/polkadot/api/query/staking/ledger.ts
+++ src/types/polkadot/api/query/staking/ledger.ts
@@ -1,7 +1,12 @@
+import { BrokenNumberType } from "@polkadot/types/common/BrokenNumberType";
+
 export type Staking_Ledger_Json = {
   stash: string;
   total: number;
   active: number;
-  unlocking: string[];
+  unlocking: {
+    value: BrokenNumberType;
+    era: number;
+  }[];
   legacyClaimedRewards: number[];
 };
