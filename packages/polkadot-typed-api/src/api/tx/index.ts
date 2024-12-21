c5777f69e02f0b4eba6070576508b703f8f1da1c
Stas Kolkovsky <stas.kolkovsky@gmail.com>
2024-12-21 04:25:07 +0300

    Add exports for utility pallet

--- packages/polkadot-typed-api/src/api/tx/index.ts
+++ packages/polkadot-typed-api/src/api/tx/index.ts
@@ -2,3 +2,4 @@ export * as balances from "./balances";
 export * as nominationPools from "./nominationPools";
 export * as staking from "./staking";
 export * as proxy from "./proxy";
+export * as utility from './utility';
